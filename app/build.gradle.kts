349bcdefa3808277e1bf737e613ba860eef48237
jjwjw05312-bit <jjwjw05312@gmail.com>
2026-06-02 13:34:16 +0000

    mmm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,4 +14,15 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
